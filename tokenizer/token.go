635297533ec8da60a8d14de37fade10453c15c1b
Tushar Jois <tjois1@jhu.edu>
2018-03-29 20:37:16 -0400

    Comments for boilerplate
    Added go doc comments to functions and structs so far. Also fixed a
    return type mismatch in the GetAll function.

--- tokenizer/token.go
+++ tokenizer/token.go
@@ -1,5 +1,10 @@
 package tokenizer
 
+// Token is a single element of the Tokenizer.
+// It contains one of 4 Kinds: chstring, numstring, special, control,
+// each of which corresponds to a kind of input readable by the server.
+// The Content of the Token corresponds to the byte(s) read by the
+// server.
 type Token struct {
 	Kind             string
 	Content          string
